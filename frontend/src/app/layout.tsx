e8b74dc7eb81e66c87388c3c7debd4ed3b3cc9bd
Richouf95 <wourichouf95@gmail.com>
2024-09-16 21:48:08 +0100

    Feat: 404 page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,6 @@
 "use server";
 import React from "react";
 import "./globals.scss";
-import { Provider } from "react-redux";
-import store from "@/store/store";
 import ThemeWrapper from "./ThemeWrapper";
 
 export default async function RootLayout({
--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 function GlobalNotFoundPage() {
+  const router = useRouter();
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 4900);
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timer);
+    };
+  }, [router]);
+
   return (
-    <div>GlobalNotFoundPage</div>
-  )
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Page not found</h1> <br />
+        <p>
+          You will be redirected to the home page in {countdown}{" "}
+          {countdown === 1 ? "second" : "seconds"}.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default GlobalNotFoundPage
+export default GlobalNotFoundPage;
